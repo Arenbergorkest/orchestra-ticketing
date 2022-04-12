38fa5584f9803174b26b5fa9ef6c78f027ba6e78
Arne De Brabandere <arne.de.brabandere@telenet.be>
2022-04-12 21:13:48 +0200

    Translation of production name and description

--- translation.py
+++ translation.py
@@ -3,6 +3,10 @@ from modeltranslation.translator import register, TranslationOptions
 from .models import Production, PriceCategory
 
 
+@register(Production)
+class ProductionTranslationOptions(TranslationOptions):
+    fields = ('name', 'description')
+
 @register(PriceCategory)
 class PriceCategoryTranslationOptions(TranslationOptions):
     fields = ('name',)
